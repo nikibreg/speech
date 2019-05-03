f7732adc532c0b672c69ddd538ebe3a28ff9a56b
David <david.benia.1@btu.edu.ge>
2019-05-03 00:33:37 -0700

    refining

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
 import speech_recognition as sr
 
-def listener():
+def recorder():
     r = sr.Recognizer()
     try:
         with sr.AudioFile('uploads/recording.wav') as source:
-            print('recording.')
             audio = r.record(source, duration=15)
-            print('done.')
         text = r.recognize_google(audio)
         print("You said : {}".format(text))
+        return text
     except sr.UnknownValueError:
         print("Sorry could not recognize what you said.")
-        reckon()
     except sr.RequestError as e:
         print("Could not request result; {0}".format(e))
 
-def reckon():
-    def user():
-        ask = input('Type START to record your voice\n>>')
-        if ask.upper() == 'START':
-            return 'starting'
-        else:
-            print('Command not recognized, try again.')
-            reckon()
-
-    if user() == 'starting':
-        listener()
-
 def main():
-    reckon()
+    
+    print('recording.')
+    result = recorder()
+    if type(result) == str:
+        print('done.')
+        return result
+    else:
+        print('try again.')
 
 main()
